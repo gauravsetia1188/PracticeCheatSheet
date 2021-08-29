738d779aea9f6881257400c2a74597c9280575e1
Gaurav Setia <gauravsetia1188@gmail.com>
2021-08-30 01:30:30 +0530

    Create ple.cpp

---
+++ ple.cpp
@@ -0,0 +1,8 @@
+vector<int> previous_less(A.size(), -1);
+for(int i = 0; i < A.size(); i++){
+  while(!in_stk.empty() && A[in_stk.top()] > A[i]){
+    in_stk.pop();
+  }
+  previous_less[i] = in_stk.empty()? -1: in_stk.top();
+  in_stk.push(i);
+}
