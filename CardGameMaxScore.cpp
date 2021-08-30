1de136c56c25bfdfdee9cc3bfcfa3a8dbf8261fc
Gaurav Setia <gauravsetia1188@gmail.com>
2021-08-31 02:50:54 +0530

    Update CardGameMaxScore.cpp

--- CardGameMaxScore.cpp
+++ CardGameMaxScore.cpp
@@ -1,6 +1,21 @@
 // variant - which player wins card game :  stone game 3
 
 
+
+// Two players are playing a card game where the deck of cards are layed out in a straight line and each card value is visible to both the players.
+// The value of each card can range from a reasonable [-ve to +ve] number and the length of deck in n.
+
+// The rules of the game are such:
+
+// Player 1 starts the game
+// Each player has 3 option:
+// (Option 1: Pick 1st card)
+// (Option 2: Pick 1st two cards)
+// (Option 3: Pick 1st three cards)
+// You're only allowed to pick cards from the left side of the deck
+// Both players have to play optimally.
+// Return the maximum sum of cards Player 1 can obtain by playing optimally.
+    
 public static int find(int[] cards) {
     if (cards == null || cards.length == 0) {
         return 0;
